738f0e42cba866b5f401b59c287d177263928ab2
JarolWilliams <215430519+jarolwilliams1@users.noreply.github.com.>
2026-08-10 17:00:20 -0400

    feat: agregar middlewares, servicios y actualizar gitignore

---
+++ middlewares/verificarSesion.js
@@ -0,0 +1,14 @@
+const session = require('express-session');
+
+async function  verificarSesion(req, res, next)
+{
+    if(req.session){
+        
+    }
+
+}
+
+
+module.exports = {
+    verificarSesion
+};
---
+++ services/crearCuneta.cliente.delivery.js
@@ -0,0 +1,10 @@
+async function ValidarDatos(datosLogin, res)
+ {
+    if(datosLogin.usuario == null || datosLogin.usuario == ' ' || datosLogin.password == null || datosLogin.password == ' ')
+    {
+        
+    }
+    
+}
+
+module.exports = {ValidarDatos}
