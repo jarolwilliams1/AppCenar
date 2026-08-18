bab75a1626d789bed5856543e9ff436c813ff41a
JarolWilliams <215430519+jarolwilliams1@users.noreply.github.com.>
2026-08-17 23:44:54 -0400

    trabajando en la creacion de pedidos

--- routers/clienteRouter.js
+++ routers/clienteRouter.js
@@ -8,5 +8,4 @@ router.get("/perfil", clienteController.perfil );
 router.get('/home/comercios/:id', clienteController.VerComerciosPorTipo);
 //router.get("/home", clienteController.Home);
 
-const docker = "dckr_pat_ECiSP7XJVvXZmiNeaZX7Ul_OGpw";
 module.exports = router;
